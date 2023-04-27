a418e7941320e4be09728f1f033768863a594c7f
Evan BOISSONNOT <evan@boissonnot.fr>
2023-04-27 19:32:22 +0200

    feat switch / merge / concat

--- quetes/quete-004/starwars-game/projects/game/src/app/learnings/from-event-operator/from-event-operator.component.ts
+++ quetes/quete-004/starwars-game/projects/game/src/app/learnings/from-event-operator/from-event-operator.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ElementRef } from '@angular/core';
 import { AfterViewInit } from '@angular/core';
 import { ViewChild } from '@angular/core';
-import { Observable, fromEvent } from 'rxjs';
+import { Observable, concatMap, fromEvent, interval, mergeMap, switchMap, take, tap } from 'rxjs';
 
 @Component({
   selector: 'game-from-event-operator',
@@ -17,7 +17,29 @@ export class FromEventOperatorComponent implements AfterViewInit {
   @ViewChild('toClick') button !: ElementRef<HTMLButtonElement>;
 
   ngAfterViewInit(): void {
-    this.timer$ = fromEvent(this.button.nativeElement, 'click');
+    // this.timer$ = fromEvent(this.button.nativeElement, 'click').pipe(
+    //   tap(item => console.info('tap', item)),
+    //   mergeMap(() => interval(1000)),
+    //   tap(item => console.info('tap 2', item))
+    // );
+
+    // this.timer$ = fromEvent(this.button.nativeElement, 'click').pipe(
+    //   tap(item => console.info('tap', item)),
+    //   concatMap(() => interval(1000).pipe(take(4))),
+    //   tap(item => console.info('tap 2', item))
+    // );
+
+    this.timer$ = fromEvent(this.button.nativeElement, 'click').pipe(
+      tap(item => console.info('tap', item)),
+      switchMap(() => interval(1000)),
+      tap(item => console.info('tap 2', item))
+    );
+
+    // this.timer$.subscribe(() => {
+    //   interval(1000).subscribe(item => {
+    //     console.info('timer', item);
+    //   });
+    // });
   }
 
   toLog(): void {
