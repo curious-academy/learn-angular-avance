9a2f705d34d7dc88b3ff52d8d0fc57c74fdedb0b
Evan BOISSONNOT <evan@boissonnot.fr>
2023-07-11 15:57:26 +0200

    feat: fin quete 004

--- quetes/quete-004/starwars-game/projects/game/src/app/features/game/game-list/game-list.component.ts
+++ quetes/quete-004/starwars-game/projects/game/src/app/features/game/game-list/game-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { Observable, concatMap, switchMap } from 'rxjs';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Observable, Subscription, concatMap, switchMap } from 'rxjs';
 import { GameDto } from '../../../core/models/game.dto';
 import { GameService } from '../services/game.service';
 import { SearchService } from '../../../shared/ui/search/services';
@@ -9,21 +9,26 @@ import { SearchService } from '../../../shared/ui/search/services';
   templateUrl: './game-list.component.html',
   styleUrls: ['./game-list.component.css']
 })
-export class GameListComponent implements OnInit {
+export class GameListComponent implements OnInit, OnDestroy {
   private readonly searchService = inject(SearchService);
   games: GameDto[] = [];
   searchItem = '';
+  private subscription = new Subscription();
 
   constructor(private gameService: GameService) { }
 
   ngOnInit(): void {
-    this.searchService.asObservable.pipe(
-      switchMap(item => this.gameService.getAll(item.value, 3))
-    )
-    .subscribe(items => {
-      this.games = items;
-    });
-
+    this.subscription.add(
+      this.searchService.asObservable.pipe(
+        switchMap(item => this.gameService.getAll(item.value, 3))
+      )
+      .subscribe(items => {
+        this.games = items;
+      })
+    );
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- quetes/quete-004/starwars-game/projects/game/src/app/features/game/services/game.service.ts
+++ quetes/quete-004/starwars-game/projects/game/src/app/features/game/services/game.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { api } from 'projects/game/src/environments/api';
-import { Observable } from 'rxjs';
+import { Observable, shareReplay } from 'rxjs';
 import { GameDto } from '../../../core/models/game.dto';
 
 @Injectable()
@@ -10,6 +10,8 @@ export class GameService {
   constructor(private httpClient: HttpClient) { }
 
   getAll(searchValue: string, nbItems: number = 100): Observable<GameDto[]> {
-    return this.httpClient.get<GameDto[]>(api.games.url + '?max=' + nbItems);
+    return this.httpClient.get<GameDto[]>(api.games.url + '?max=' + nbItems).pipe(
+      shareReplay(1)
+    );
   }
 }
