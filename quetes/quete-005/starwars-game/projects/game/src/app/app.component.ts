1ded411c5bd7af39862a4f9dbe7bc40ba74f1a2e
Evan BOISSONNOT <evan@boissonnot.fr>
2023-07-12 16:00:30 +0200

    feat: migration finie et fonctionnelle

--- quetes/quete-005/starwars-game/projects/game/src/app/app.component.ts
+++ quetes/quete-005/starwars-game/projects/game/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserListComponent } from './features/user/user-list/user-list.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
 
 @Component({
     selector: 'game-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
     standalone: true,
-    imports: [UserListComponent, RouterOutlet]
+    imports: [
+      HeaderComponent,
+      SideBarComponent,
+      UserListComponent,
+      RouterOutlet
+    ]
 })
 export class AppComponent {
   title = 'game';
